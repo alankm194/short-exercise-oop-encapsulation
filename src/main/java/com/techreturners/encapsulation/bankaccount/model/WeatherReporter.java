c1c005b6f7a0d4777e1f8745b08efd601f9a39fd
alankm194 <alankm194@gmail.com>
2023-01-30 16:43:21 +0000

    - remove static instantiation of Enum map of location and weather to WeatherReporter class - remove enum-map locationWeatherMap from enum location to WeatherReporter class

--- src/main/java/com/techreturners/encapsulation/bankaccount/model/WeatherReporter.java
+++ src/main/java/com/techreturners/encapsulation/bankaccount/model/WeatherReporter.java
@@ -12,30 +12,24 @@ public class WeatherReporter {
     private final static int MAX_TEMP_IN_CELSIUS= 30;
     private final static int MIN_TEMP_IN_CELSIUS= 10;
     private final static String DEFAULT_WEATHER = "🔆";
-    private enum LOCATION {
+    private final static Map<LOCATION, String> locationWeatherMap;
+
+    static {
+        locationWeatherMap = new EnumMap<>(LOCATION.class);
+        locationWeatherMap.put(LOCATION.LONDON, "🌦");
+        locationWeatherMap.put(LOCATION.CAPE_TOWN, "🌅");
+        locationWeatherMap.put(LOCATION.CALIFORNIA, "🌤");
+    }
+
+    enum LOCATION {
         LONDON("London"),
         CAPE_TOWN("Cape Town"),
         CALIFORNIA("California");
 
         private final String label;
-        private final static Map<LOCATION, String> locationWeatherMap;
 
         LOCATION(String label) {
            this.label = label;
-        }
-        static {
-            locationWeatherMap = new EnumMap<>(LOCATION.class);
-            locationWeatherMap.put(LOCATION.LONDON, "🌦");
-            locationWeatherMap.put(LOCATION.CAPE_TOWN, "🌅");
-            locationWeatherMap.put(LOCATION.CALIFORNIA, "🌤");
-        }
-
-        public static String getWeatherFromLocation(String location) {
-            return Arrays.stream(LOCATION.values())
-                    .filter(enumLocation -> enumLocation.label.equalsIgnoreCase(location))
-                    .findFirst()
-                    .map(locationWeatherMap::get)
-                    .orElse(DEFAULT_WEATHER);
         }
     }
 
@@ -49,12 +43,20 @@ public class WeatherReporter {
         return (9.0 / 5.0) * temp + 32;
     }
     public String print() {
-        double newTemp = convertCelsiusToFahrenheit(temperatureInCelsius);
+        double newTempFahrenheit = convertCelsiusToFahrenheit(temperatureInCelsius);
         return MessageFormat.format("I am in {0} and it is {1}. {2}. The temperature in Fahrenheit is {3}.",
                 location,
-                LOCATION.getWeatherFromLocation(location),
+                getWeatherFromLocation(location),
                 getResponseFromTemperature(),
-                newTemp);
+                newTempFahrenheit);
+    }
+
+    private static String getWeatherFromLocation(String location) {
+        return Arrays.stream(LOCATION.values())
+                .filter(enumLocation -> enumLocation.label.equalsIgnoreCase(location))
+                .findFirst()
+                .map(locationWeatherMap::get)
+                .orElse(DEFAULT_WEATHER);
     }
 
 
